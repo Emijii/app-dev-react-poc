27f72db7c52929100398c8ab6b1a359d27d61ee8
emily.jimmerson <57643494+Emijii@users.noreply.github.com>
2021-02-08 09:43:36 -0700

    Edit progress

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -8,15 +8,17 @@ import ClearIcon from '@material-ui/icons/Clear';
 export default function Edit() {
 
     const name = "Mochi";
+    const type = "Australian Kelpie";
+    const application = "Herding";
+    const legendTitle = "Mochi is the best!";
+    const fileName = "Mochi.png";
+    const imageStatus = "Active";
 
     const classes = useStyles();
 
     return (
         <div>
             <Paper className={classes.paper}>
-                <Typography gutterBottom variant="subtitle1">
-                    <TextField label="Name" variant="outlined" value={name || ''} />
-                </Typography>
                 <Grid item xs={12} container spacing={2}>
                     <Grid item>
                         <ButtonBase className={classes.image}>
@@ -26,43 +28,33 @@ export default function Edit() {
                     <Grid item xs container direction="column" spacing={2}>
                         <Grid container spacing={1}>
                             <Grid container item xs={12} spacing={3}>
-                                <Grid item xs={4}>
-                                    <Typography>TYPE</Typography>
-                                </Grid>
-                                <Grid item xs={8}>
-                                    <Typography>Australian Kelpie</Typography>
+                                <Grid item xs={12}>
+                                    <TextField label="Name" variant="outlined" defaultValue={name || ''} />
                                 </Grid>
                             </Grid>
                             <Grid container item xs={12} spacing={3}>
-                                <Grid item xs={4}>
-                                    <Typography>APPLICATION</Typography>
-                                </Grid>
-                                <Grid item xs={8}>
-                                    <Typography>Service</Typography>
+                                <Grid item xs={12}>
+                                    <TextField label="Type" variant="outlined" defaultValue={type || ''} />
                                 </Grid>
                             </Grid>
                             <Grid container item xs={12} spacing={3}>
-                                <Grid item xs={4}>
-                                    <Typography>LEGEND TITLE</Typography>
-                                </Grid>
-                                <Grid item xs={8}>
-                                    <Typography>Mochi is the best!</Typography>
+                                <Grid item xs={12}>
+                                    <TextField label="Application" variant="outlined" defaultValue={application || ''} />
                                 </Grid>
                             </Grid>
                             <Grid container item xs={12} spacing={3}>
-                                <Grid item xs={4}>
-                                    <Typography>FILE NAME</Typography>
-                                </Grid>
-                                <Grid item xs={8}>
-                                    <Typography>Mochi.jpg</Typography>
+                                <Grid item xs={12}>
+                                    <TextField label="Legend Title" variant="outlined" defaultValue={legendTitle || ''} />
                                 </Grid>
                             </Grid>
                             <Grid container item xs={12} spacing={3}>
-                                <Grid item xs={4}>
-                                    <Typography>IMAGE STATUS</Typography>
+                                <Grid item xs={12}>
+                                    <TextField label="File Name" variant="outlined" defaultValue={fileName || ''} />
                                 </Grid>
-                                <Grid item xs={8}>
-                                    <Typography>Active</Typography>
+                            </Grid>
+                            <Grid container item xs={12} spacing={3}>
+                                <Grid item xs={12}>
+                                    <TextField label="Image Status" variant="outlined" defaultValue={imageStatus || ''} />
                                 </Grid>
                             </Grid>
                         </Grid>
