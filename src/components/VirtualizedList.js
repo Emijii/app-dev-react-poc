091903ad38a59ceb998745761b1cc43c8943de9a
emily.jimmerson <57643494+Emijii@users.noreply.github.com>
2021-02-23 14:35:05 -0700

    FixedSizeGrid virtualization progress

--- src/components/VirtualizedList.js
+++ src/components/VirtualizedList.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { FixedSizeList } from 'react-window';
+import { FixedSizeGrid } from 'react-window';
 import ItemContext from './context/ItemContext';
 
 export default function VirtualizedList() {
@@ -10,9 +11,25 @@ export default function VirtualizedList() {
 
     return (
         <div className={classes.root}>
+            <FixedSizeGrid itemData={items} columnCount={3} columnWidth={100} height={150} rowCount={Math.max(items.length / 3)} rowHeight={35} width={300}>
+                {Cell}
+            </FixedSizeGrid>
             <FixedSizeList itemData={items} itemSize={46} itemCount={items.length} height={400} width={300}>
                 {ItemRenderer}
             </FixedSizeList>
+        </div>
+    );
+};
+
+function Cell({ columnIndex, rowIndex, style, data }) {
+
+    const item = data[rowIndex * 3 + columnIndex];
+
+    console.log("item: " + item.name);
+
+    return (
+        <div style={style}>
+            {item.name} {rowIndex},{columnIndex} {data.name}
         </div>
     );
 };
